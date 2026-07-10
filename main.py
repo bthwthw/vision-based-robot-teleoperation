328311c3b79f93f046ab94e481fe5a6bde5da8fb
bthwthw <bachthu.du8065@gmail.com>
2026-07-10 10:15:14 +0700

    data

--- main.py
+++ main.py
@@ -25,9 +25,9 @@ def draw_3d_axes(image, intrinsics, origin_3d, rot_matrix, axis_length=0.015):
         uy, vy = rs.rs2_project_point_to_pixel(intrinsics, y_3d.tolist())
         uz, vz = rs.rs2_project_point_to_pixel(intrinsics, z_3d.tolist())
         
-        cv2.line(image, p0, (int(ux), int(vx)), (0, 0, 255), 3) # X - Đỏ
         cv2.line(image, p0, (int(uy), int(vy)), (0, 255, 0), 3) # Y - Lá
         cv2.line(image, p0, (int(uz), int(vz)), (255, 0, 0), 3) # Z - Lam
+        cv2.line(image, p0, (int(ux), int(vx)), (0, 0, 255), 3) # X - Đỏ
     except Exception as e:
         pass
     return image
@@ -40,10 +40,6 @@ def draw_axes_legend(image):
     cv2.putText(image, "Coordinate System:", (start_x, start_y), 
                 cv2.FONT_HERSHEY_SIMPLEX, 0.6, (200, 200, 200), 2, cv2.LINE_AA)
     
-    cv2.line(image, (start_x, start_y + 25), (start_x + 30, start_y + 25), (0, 0, 255), 3)
-    cv2.putText(image, "X", (start_x + 40, start_y + 30), 
-                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2, cv2.LINE_AA)
-    
     cv2.line(image, (start_x, start_y + 55), (start_x + 30, start_y + 55), (0, 255, 0), 3)
     cv2.putText(image, "Y", (start_x + 40, start_y + 60), 
                 cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2, cv2.LINE_AA)
@@ -51,6 +47,10 @@ def draw_axes_legend(image):
     cv2.line(image, (start_x, start_y + 85), (start_x + 30, start_y + 85), (255, 0, 0), 3)
     cv2.putText(image, "Z", (start_x + 40, start_y + 90), 
                 cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2, cv2.LINE_AA)
+    
+    cv2.line(image, (start_x, start_y + 25), (start_x + 30, start_y + 25), (0, 0, 255), 3)
+    cv2.putText(image, "X", (start_x + 40, start_y + 30), 
+                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2, cv2.LINE_AA)
     
     return image
 
@@ -133,7 +133,7 @@ def main():
                         P_TCP_3D = tcp_filter.filter(P_TCP_3D, timestamp / 1000.0)
                         u_disp, v_disp = rs.rs2_project_point_to_pixel(camera.intrinsics, P_TCP_3D)
                         cv2.circle(color_img, (uTCP, vTCP), 8, (0, 255, 255), cv2.FILLED)
-                        cv2.circle(color_img, (int(u_disp), int(v_disp)), 8, (0, 0, 255), cv2.FILLED)
+                        cv2.circle(color_img, (int(u_disp), int(v_disp)), 8, (255, 0, 255), cv2.FILLED)
                         
                         tcp_text = f"TCP Pos: X:{P_TCP_3D[0]:.3f} Y:{P_TCP_3D[1]:.3f} Z:{P_TCP_3D[2]:.3f} m"
                         cv2.putText(color_img, tcp_text, (20, 50), 
