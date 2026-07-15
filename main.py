0ddbcf77ede5048a7ad04fbfd6ca1f3e66ef8aad
bthwthw <bachthu.du8065@gmail.com>
2026-07-15 09:58:03 +0700

    tunning: ori filter - max rejects

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ def main():
     camera = RealSenseNode(playback_file=playback_file)
     tracker = HandTrackerNode(model_path='model/hand_landmarker.task')
     tcp_filter = Position3DFilter(min_cutoff=0.1, beta=15.0, cutoff_max=15.0, reject_max_jump_mps=2.5)
-    quat_filter = QuaternionFilter(min_cutoff=1.5, beta=1.0, cutoff_max=20.0, reject_max_omega=15.0)
+    quat_filter = QuaternionFilter(min_cutoff=1.5, beta=1.0, cutoff_max=20.0, reject_max_omega=15.0, max_rejects=20)
     gripper_filter = Scalar1DFilter(min_cutoff=1.0, beta=0.002, cutoff_max=10.0, reject_max_jump=3500.0)
     
     current_time_str = datetime.now().strftime("%Y%m%d_%H%M%S")
